b30050cbc365e20d6ef82a45a1990fbe55791ee7
Craig Schubert <craig.schubert@gmail.com>
2020-01-15 18:21:46 +1030

    Initial Commit

---
+++ HwS Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HwS Project 1
+//
+//  Created by Craig Schubert on 15/1/20.
+//  Copyright © 2020 Craig Schubert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
